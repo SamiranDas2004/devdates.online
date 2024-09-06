ac6dc274730f6a02156ea1109c84b0939698e9e3
Samiran_Das <samiran4209@gmail.com>
2024-09-06 07:53:54 +0530

    ""

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,8 +1,8 @@
-// import { io } from 'socket.io-client';
+import { io } from 'socket.io-client';
 
-// // Get the SOCKET_URL from environment variables
-// const socketUrl = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:3001';
+// Get the SOCKET_URL from environment variables
+const socketUrl = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:3001';
 
-// export const socket = io(socketUrl, {
-//   transports: ['websocket', 'polling'],
-// });
+export const socket = io(socketUrl, {
+  transports: ['websocket', 'polling'],
+});
