1ebfc253816957941d039845754ba3e65e345117
Samiran_Das <samiran4209@gmail.com>
2024-09-06 07:46:22 +0530

    ""

--- src/server.js
+++ src/server.js
@@ -1,55 +1,55 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
+// const express = require('express');
+// const http = require('http');
+// const { Server } = require('socket.io');
+// const cors = require('cors');
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-  },
-});
+// const app = express();
+// const server = http.createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: '*',
+//     methods: ['GET', 'POST'],
+//   },
+// });
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-const activeUsers = {};
-console.log("active users",activeUsers);
+// const activeUsers = {};
+// console.log("active users",activeUsers);
 
-io.on('connection', (socket) => {
-  console.log('a user connected:', socket.id);
+// io.on('connection', (socket) => {
+//   console.log('a user connected:', socket.id);
 
-  socket.on('joinRoom', (email) => {
-    activeUsers[email] = socket.id;
-    console.log(`${email} joined with socket ID ${socket.id}`);
-    console.log('Active users:', activeUsers); 
-  });
+//   socket.on('joinRoom', (email) => {
+//     activeUsers[email] = socket.id;
+//     console.log(`${email} joined with socket ID ${socket.id}`);
+//     console.log('Active users:', activeUsers); 
+//   });
 
-  socket.on('sendMessage', ({ fromUser, toUser, message }) => {
-    const toSocketId = activeUsers[toUser];
+//   socket.on('sendMessage', ({ fromUser, toUser, message }) => {
+//     const toSocketId = activeUsers[toUser];
   
 
-    if (toSocketId) {
-      io.to(toSocketId).emit('receiveMessage', { fromUser, message });
-    } else {
-      console.log('User not found or not connected:', toUser);
-    }
-  });
+//     if (toSocketId) {
+//       io.to(toSocketId).emit('receiveMessage', { fromUser, message });
+//     } else {
+//       console.log('User not found or not connected:', toUser);
+//     }
+//   });
 
-  socket.on('disconnect', () => {
-    for (const email in activeUsers) {
-      if (activeUsers[email] === socket.id) {
-        delete activeUsers[email];
-        break;
-      }
-    }
-    console.log('a user disconnected:', socket.id);
-    console.log('Active users:', activeUsers); // Log active users
-  });
-});
+//   socket.on('disconnect', () => {
+//     for (const email in activeUsers) {
+//       if (activeUsers[email] === socket.id) {
+//         delete activeUsers[email];
+//         break;
+//       }
+//     }
+//     console.log('a user disconnected:', socket.id);
+//     console.log('Active users:', activeUsers); // Log active users
+//   });
+// });
 
-server.listen(3001, () => {
-  console.log('listening on *:3001');
-});
+// server.listen(3001, () => {
+//   console.log('listening on *:3001');
+// });
